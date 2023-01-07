c5d2d747210080142d7db0bdb48c7deb8ea74073
lienism <didwhdqls7@gmail.com>
2023-01-07 12:49:16 +0900

    first

---
+++ ImageDiff/Diff_Form.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ImageDiff
+{
+    partial class Diff_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Load_Button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.Save_Button = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Load_Button
+            // 
+            this.Load_Button.Location = new System.Drawing.Point(332, 56);
+            this.Load_Button.Name = "Load_Button";
+            this.Load_Button.Size = new System.Drawing.Size(75, 23);
+            this.Load_Button.TabIndex = 0;
+            this.Load_Button.Text = "Load";
+            this.Load_Button.UseVisualStyleBackColor = true;
+            this.Load_Button.Click += new System.EventHandler(this.Load_Button_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(289, 235);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(456, 23);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(289, 235);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(226, 296);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(289, 235);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Save_Button
+            // 
+            this.Save_Button.Location = new System.Drawing.Point(332, 200);
+            this.Save_Button.Name = "Save_Button";
+            this.Save_Button.Size = new System.Drawing.Size(75, 23);
+            this.Save_Button.TabIndex = 4;
+            this.Save_Button.Text = "Save";
+            this.Save_Button.UseVisualStyleBackColor = true;
+            this.Save_Button.Click += new System.EventHandler(this.Save_Button_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Diff_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 577);
+            this.Controls.Add(this.Save_Button);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Load_Button);
+            this.Name = "Diff_Form";
+            this.Text = "Diff_Form";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Load_Button;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button Save_Button;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ ImageDiff/Diff_Form.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace ImageDiff
+{
+    public partial class Diff_Form : Form
+    {
+        Mat isLeftMat;
+        Mat isRightMat;
+        public Diff_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Load_Button_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                this.isLeftMat = Cv2.ImRead(openFileDialog1.FileName);
+                this.isRightMat = Cv2.ImRead(openFileDialog2.FileName);
+                pictureBox3.Image = CompareImage(this.isLeftMat, this.isRightMat);
+                pictureBox2.Image = isRightMat.ToBitmap();
+                pictureBox1.Image = isLeftMat.ToBitmap();
+            }
+        }
+
+        private void Save_Button_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "画像ファイル(*.jpg)|*.jpg|画像ファイル(*.png)|*.png";
+            string[] Filesaved = { ".jpg", ".png" };
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap Savebmp = new Bitmap(pictureBox3.Image);
+                Savebmp.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        /// <summary>
+        /// 比較して結果を出す
+        /// </summary>
+        /// <param name="Left">左の画像</param>
+        /// <param name="Right">右の画像</param>
+        /// <returns>比較した結果</returns>
+        private Bitmap CompareImage(Mat Left, Mat Right)
+        {
+            Mat Result = new Mat();
+            Cv2.Absdiff(Left, Right, Result);
+            Result = BlurProcess(Result);
+            Result = Diff_Red_Draw(Result, Left);
+            return Result.ToBitmap();
+        }
+        /// <summary>
+        /// ノイズ削除
+        /// </summary>
+        /// <param name="src"></param>
+        /// <returns></returns>
+        private Mat BlurProcess(Mat src)
+        {
+            Cv2.Dilate(src, src, new Mat(new OpenCvSharp.Size(11, 11), MatType.CV_8UC1));
+            Cv2.Erode(src, src, new Mat(new OpenCvSharp.Size(11, 11), MatType.CV_8UC1));
+            return src;
+        }
+
+        /// <summary>
+        /// 差分に赤色を描きます
+        /// </summary>
+        /// <param name="src">差分のイメージ</param>
+        /// <returns>赤色の差分表示</returns>
+        private Mat Diff_Red_Draw(Mat src, Mat left)
+        {
+            Mat Original = left;
+            Mat result = new Mat();
+            OpenCvSharp.Point[][] contours;
+            HierarchyIndex[] hi;
+            src = Gray(src);
+            Cv2.Threshold(src, src, 0, 255, ThresholdTypes.Otsu);
+
+            Cv2.FindContours(src, out contours, out hi, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+
+            result = Diff_Draw(contours, Original);
+
+            return result;
+        }
+
+        private Mat Diff_Draw(OpenCvSharp.Point[][] contours, Mat src)
+        {
+            RotatedRect RoRect;
+            Rect Rect;
+            for(int i = 0; i < contours.Length; i++)
+            {
+                RoRect = Cv2.MinAreaRect(contours[i]);
+                Rect = RoRect.BoundingRect();
+                Cv2.Rectangle(src, Rect, new Scalar(0, 0, 255), 4);
+            }
+            return src;
+        }
+
+
+        /// <summary>
+        /// Gray化する関数
+        /// </summary>
+        /// <param name="src">オリジナル画像</param>
+        /// <returns>グレー画像</returns>
+        private Mat Gray(Mat src)
+        {
+            Cv2.CvtColor(src, src, ColorConversionCodes.BGR2GRAY);
+            return src;
+        }
+    }
+}
